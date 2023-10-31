263d87ac1903de07fd7997ecdde48f2d8e66136a
MagfirohIndahKarisma <magfirohindah33@gmail.com>
2023-10-31 08:45:55 +0700

    w8

--- week8/belanja/test/widget_test.dart
+++ week8/belanja/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:assignment1_layoutbasics/widget/putTogether.dart';
-//import 'package:assignment1_layoutbasics/main.dart';
+
+import 'package:belanja/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyWidget());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
